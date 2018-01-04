12c4040f40cc23b37526aecb8f6b63a8db3684ee
Matthew Pickering <matthewtpickering@gmail.com>
2018-01-04 11:51:39 +0000

    Add instructions and scripts for using the repo on nix

---
+++ scripts/overrides.nix
@@ -0,0 +1,42 @@
+{ stdenv
+  , haskell
+  , patches  # A directory containing patch files used to build packages
+             # it can either be a local directory or fetched from the web
+}:
+let
+  ghc = haskell.packages.ghc822.ghcWithPackages (ps: with ps;
+        [ ps.hopenssl ps.distribution-nixpkgs
+        ]);
+in
+
+stdenv.mkDerivation {
+
+  name = "hs-generate-overrides-0.1";
+
+  src = ./generate-nix-overrides.hs;
+
+  preUnpack = ''mkdir hs-generate-overrides'';
+  buildInputs = [ ghc ];
+
+  unpackCmd = ''
+    cp $curSrc ./hs-generate-overrides
+    cp -r ${patches} ./hs-generate-overrides/patches
+    sourceRoot=hs-generate-overrides;
+  '';
+
+  buildPhase = ''
+    ghc $src -o generate
+    ./generate $script/patches patches > patches.nix
+  '';
+
+  outputs = ["out" "script"];
+
+  installPhase = ''
+    cp patches.nix $out
+    ensureDir $script/patches
+    cp -r patches $script/patches
+  '';
+
+}
+
+
