ec9ca68e8254c9563c18ffbd6d9e3f84ed3f5584
Matthew Pickering <matthewtpickering@gmail.com>
2018-08-15 19:21:39 +0000

    Properly handle .cabal files as well as patches

--- scripts/generate-nix-overrides.hs
+++ scripts/generate-nix-overrides.hs
@@ -42,12 +42,27 @@ mkOverride (display -> pName, patches) =
 
 generateOverride :: FilePath -> FilePath -> FilePath -> IO (PackageName, ([Int], String))
 generateOverride prefix patchDir patch = do
-  let pid' :: Maybe PackageId  = simpleParse (takeBaseName patch)
+  let ftype = decidePatchType patch
+      pid' :: Maybe PackageId  = simpleParse (takeBaseName patch)
   pid <- maybe (fail ("invalid patch file name: " ++ show patch)) return pid'
   let pname = display (packageName pid)
       version = versionNumbers (packageVersion pid)
   return $ (packageName pid, (version,
-    unwords [ "dontRevise(", "haskell.lib.appendPatch", "super."++pname, prefix </> patchDir </> patch, ")"]))
+    unwords [ "dontRevise(", patchFunction ftype , "super."++pname, prefix </> patchDir </> patch, ")"]))
+
+
+patchFunction :: PatchType -> String
+patchFunction CabalPatch = "setCabalFile"
+patchFunction NormalPatch = "haskell.lib.appendPatch"
+
+
+decidePatchType :: FilePath -> PatchType
+decidePatchType patch =
+  case takeExtension patch of
+    ".cabal" -> CabalPatch
+    _ -> NormalPatch
+
+data PatchType = CabalPatch | NormalPatch
 
 main :: IO ()
 main = do
@@ -60,6 +75,7 @@ main = do
   overrides <- generateOverrides prefix patchDir
   putStrLn "{haskell}:"
   putStrLn "let dontRevise = pkg: haskell.lib.overrideCabal pkg (old: { editedCabalFile = null; }); in"
+  putStrLn "let setCabalFile = pkg: file: haskell.lib.overrideCabal pkg (old: { postPatch = ''cp ${file} ${old.pname}.cabal''; }); in"
   putStrLn "self: super: {\n"
   putStrLn overrides
   putStrLn "}"
